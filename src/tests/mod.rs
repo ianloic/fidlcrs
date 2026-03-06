e0579410f7a1d28f952904685e0a689387e4ea95
Ian McKellar <ian@mckellar.org>
2026-03-06 10:40:50 -0800

    Port generated_name_tests.cc to Rust

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -14,6 +14,7 @@ pub mod errcat_good_tests;
 pub mod errors_tests;
 pub mod flat_ast_tests;
 pub mod flexible_tests;
+pub mod generated_name_tests;
 pub mod handle_tests;
 pub mod method_tests;
 pub mod parsing_tests;

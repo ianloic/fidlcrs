d5f1c607a61c79728aa877b32d402b288b82d21d
Ian McKellar <ianloic@google.com>
2026-03-06 09:39:28 -0800

    bind mount errcat docs

--- bind-mount.sh
+++ bind-mount.sh
@@ -4,14 +4,25 @@ set -e
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-function bind_mount() {
+if [[ "$1" == "unmount" || "$1" == "umount" ]]; then
+  ACTION="unmount"
+else
+  ACTION="mount"
+fi
+
+function process_dir() {
   FUCHSIA_SUBDIR="$1"
   MOUNT_SUBDIR="$2"
-  mkdir -p "$SCRIPT_DIR/$MOUNT_SUBDIR"
-  sudo mount --bind -o ro ~/fuchsia/$FUCHSIA_SUBDIR "$SCRIPT_DIR/$MOUNT_SUBDIR"
+  if [[ "$ACTION" == "unmount" ]]; then
+    sudo umount "$SCRIPT_DIR/$MOUNT_SUBDIR"
+  else
+    mkdir -p "$SCRIPT_DIR/$MOUNT_SUBDIR"
+    sudo mount --bind -o ro ~/fuchsia/$FUCHSIA_SUBDIR "$SCRIPT_DIR/$MOUNT_SUBDIR"
+  fi
 }
 
-bind_mount "tools/fidl/fidlc" "fidlc"
-bind_mount "sdk/fidl" "sdk-fidl"
-bind_mount "out/default/fidling/gen/sdk/fidl" "sdk-fidl-gen"
-bind_mount "zircon/vdso" "vdso-fidl"
+process_dir "tools/fidl/fidlc" "fidlc"
+process_dir "sdk/fidl" "sdk-fidl"
+process_dir "out/default/fidling/gen/sdk/fidl" "sdk-fidl-gen"
+process_dir "zircon/vdso" "vdso-fidl"
+process_dir "docs/reference/fidl/language/error-catalog" "errcat-docs"
